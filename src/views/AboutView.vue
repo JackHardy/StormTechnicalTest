d911b6cdc3079b2f7e5c3ffd8aceccb4fd03303b
Jack Hardy <jackhardy1990@hotmail.co.uk>
2025-02-27 08:12:27 +0000

    About section added

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="flex justify-center mt-3">
-    <h1 class="text-xl font-medium text-gray-800">This is an about page</h1>
+  <div class="flex flex-col justify-items-center mt-6 w-full gap-4 max-w-3xl mx-auto text-center">
+    <img src="https://media.licdn.com/dms/image/v2/C4E03AQHYhYcmQFdJLA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1663110851927?e=1746057600&v=beta&t=3vn-YYOBUUgB8gnh3xM4pAwDoNIso-7EfnC7Qw1Q-xc" class="grayscale size-36 rounded-full mx-auto">
+    <h1 class="text-2xl font-medium text-gray-800 mx-auto">Jack Hardy</h1>
+    <p>
+      I'm a Laravel and Vue developer with a strong passion for front-end development, shaped by my background in illustration. My focus is on crafting visually compelling and intuitive user interfaces that enhance the overall user experience. I strive to bring design and functionality together, ensuring smooth, seamless interactions that make end users feel engaged. With an eye for detail I bring creativity and precision to every project I work on.
+    </p>
+    <p>
+      I hope you enjoyed my demo To-Do project! You can move items between "To Do," "In Progress," and "Complete," create new tasks, edit existing ones (as long as they’re not complete), and delete them if needed. Clicking on a card expands it for more details, and the system provides warnings when a task is due today or overdue. I aimed to showcase my skills within a short timeframe while delivering a functional and visually appealing design. I appreciate your time and feedback!
+    </p>
   </div>
 </template>
